5936826c342d363a537714c9def334c09a9f5cb1
JosePech <jrocks169@gmail.com>
2015-03-25 21:17:18 +0100

    Cambiar el metodo factoria a crear objetos de las clases nuevas dependiedo del type code

--- src/intervals/Interval.java
+++ src/intervals/Interval.java
@@ -9,7 +9,18 @@ public class Interval {
     private Opening opening;
     
     public static Interval create(double minimum, double maximum, Opening opening) {
-        return new Interval(minimum, maximum, opening);
+        switch (opening) {
+        case BOTH_OPENED:
+            return new BothOpenedInterval(minimum, maximum);
+        case LEFT_OPENED:
+            return new LeftOpenedInterval(minimum, maximum);
+        case RIGHT_OPENED:
+            return new RightOpenedInterval(minimum, maximum);
+        case UNOPENED:
+            return new UnopenedInterval(minimum, maximum);
+        default:
+            return null;
+        }
     }
 
     private Interval(double minimum, double maximum, Opening opening) {
